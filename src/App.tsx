ad273f201dc84a77601c79670aea5701bd3d538d
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-04-27 15:35:46 -0700

    tmp Save

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Header from "./components/header/Header";
 import styles from "./App.module.scss";
+import Header from "./components/header/Header";
+import TaskForm from "./features/task/taskForm/TaskForm";
+import TaskList from "./features/task/taskList/TaskList";
 
 const App: React.FC = () => {
   return (
     <div className={styles.root}>
       <div className={styles.wrapper}>
         <Header />
+        <TaskForm />
+        <TaskList />
       </div>
     </div>
   );
--- src/features/task/taskForm/TaskForm.tsx
+++ src/features/task/taskForm/TaskForm.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import TextField from "@material-ui/core/TextField";
-import styles from "./Task.module.scss";
+import styles from "./TaskForm.module.scss";
 
-const Header: React.FC = () => {
+type Inputs = {
+  taskTitle: string;
+};
+
+const TaskForm: React.FC = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const handleCreate = (data: Inputs) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className={styles.root}>
-      <form className={styles.form} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="New Task" variant="outlined" />
+      <form onSubmit={handleSubmit(handleCreate)} className={styles.form}>
+        <TextField
+          id="outlined-basic"
+          label="New Task"
+          variant="outlined"
+          inputRef={register}
+          name="taskTitle"
+          className={styles.text_field}
+        />
       </form>
     </div>
   );
 };
 
-export default Header;
+export default TaskForm;
