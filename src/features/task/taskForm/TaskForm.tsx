db5048b8bae94e1cb50df6b72e04cc377418fe70
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-04-28 00:50:38 -0700

    Add css

--- src/features/task/taskForm/TaskForm.tsx
+++ src/features/task/taskForm/TaskForm.tsx
@@ -9,7 +9,11 @@ type Inputs = {
   taskTitle: string;
 };
 
-const TaskForm: React.FC = () => {
+type PropTypes = {
+  edit?: boolean;
+};
+
+const TaskForm: React.FC<PropTypes> = ({ edit }) => {
   const dispatch = useDispatch();
   const { register, handleSubmit, reset } = useForm();
   const handleCreate = (data: Inputs) => {
@@ -21,13 +25,23 @@ const TaskForm: React.FC = () => {
       <form onSubmit={handleSubmit(handleCreate)} className={styles.form}>
         <TextField
           id="outlined-basic"
-          label="New Task"
+          label={edit ? "Edit Task" : "New Task"}
           variant="outlined"
           // inputRef={register}
           // name="taskTitle"
           className={styles.text_field}
           {...register("taskTitle")}
         />
+        {edit ? (
+          <div className={styles.button_wrapper}>
+            <button type="submit" className={styles.submit_button}>
+              Submit
+            </button>
+            <button type="button" className={styles.cancel_button}>
+              Cancel
+            </button>
+          </div>
+        ) : null}
       </form>
     </div>
   );
