18b3d34ede202a8d5ba274bffc5cabfd0ad29ca6
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-04-27 17:13:39 -0700

    Add tasks

--- src/features/task/taskForm/TaskForm.tsx
+++ src/features/task/taskForm/TaskForm.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import TextField from "@material-ui/core/TextField";
+import { createTask } from "../taskSlice";
 import styles from "./TaskForm.module.scss";
 
 type Inputs = {
@@ -8,24 +10,24 @@ type Inputs = {
 };
 
 const TaskForm: React.FC = () => {
+  const dispatch = useDispatch();
   const { register, handleSubmit, reset } = useForm();
   const handleCreate = (data: Inputs) => {
-    console.log(data);
+    dispatch(createTask(data.taskTitle));
     reset();
   };
-
   return (
     <div className={styles.root}>
-      {/* <form onSubmit={handleSubmit(handleCreate)} className={styles.form}>
+      <form onSubmit={handleSubmit(handleCreate)} className={styles.form}>
         <TextField
           id="outlined-basic"
           label="New Task"
           variant="outlined"
-          innerRef={register}
+          // inputRef={register}
           name="taskTitle"
           className={styles.text_field}
         />
-      </form> */}
+      </form>
     </div>
   );
 };
